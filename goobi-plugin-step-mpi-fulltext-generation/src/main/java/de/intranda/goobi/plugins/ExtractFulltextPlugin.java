c58d5f19fba75fb53173e9b8a38289506b12eea5
Robert Sehr <robert.sehr@intranda.com>
2020-05-26 12:37:32 +0200

    ignore page area images

--- goobi-plugin-step-mpi-fulltext-generation/src/main/java/de/intranda/goobi/plugins/ExtractFulltextPlugin.java
+++ goobi-plugin-step-mpi-fulltext-generation/src/main/java/de/intranda/goobi/plugins/ExtractFulltextPlugin.java
@@ -153,8 +153,20 @@ public class ExtractFulltextPlugin implements IStepPluginVersion2 {
 
                 // check if file name matches pb id, get filename from facs attibute
                 int imageNumberCounter = 0;
+
+                int expextedNumberOfPages = 0;
+                for (DocStruct page : pages) {
+                    String imageName = page.getImageName();
+                    if (!imageName.startsWith("obj_img")) {
+                        expextedNumberOfPages++;
+                    }
+                }
                 for (DocStruct page : pages) {
                     String imageNo = null;
+                    String imageName = page.getImageName();
+                    if (imageName.startsWith("obj_img")) {
+                        continue;
+                    }
                     for (Metadata md : page.getAllMetadata()) {
                         if (md.getType().getName().equals("physPageNumber")) {
                             imageNo = md.getValue();
@@ -206,7 +218,7 @@ public class ExtractFulltextPlugin implements IStepPluginVersion2 {
                         }
 
                     }
-                    if (foundFile == null && pages.size() == createdFiles.size()) {
+                    if (foundFile == null && expextedNumberOfPages == createdFiles.size()) {
                         foundFile = createdFiles.get(imageNumberCounter);
                     }
                     imageNumberCounter++;
